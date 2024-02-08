c70c869aad29a1b98683341e6d6c7bfaaea6cf27
aridguy <rotimiariyo@gmail.com>
2024-02-08 19:55:20 +0100

    modified the application and added some othe components to the application, added a note component to thee application which helps to displat the newest update son load of the application, added other components to the application thats helps

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Component/Navbar";
 import Landing from "../Component/Landing";
 import Blogs from "./Blogs";
+import Note from "../Component/Notifications/Note";
 // import Footer from "../Component/Footer";
 // import Socials from "../Component/Socials";
 
@@ -9,12 +10,19 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-     
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+           <div className="bg-warning text-white">
+           <Note />
+           </div>
+          </div>
+        </div>
+      </div>
       <div className="container">
         <Landing />
       </div>
       <Blogs />
-      
     </div>
   );
 };
